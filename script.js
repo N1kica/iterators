6c810b448e6b1dbb132bfbfebc32a80bdcef5771
N1kica <68340084+N1kica@users.noreply.github.com>
2024-06-12 12:30:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,243 +1,192 @@
-class Player {
-  /** @type {number} */
-  x;
-
-  /** @type {number} */
-  y;
-
-  constructor() {
-    this.x = 0;
-    this.y = 0;
-
-    const span = document.createElement("span");
-
-    span.id = "player";
-    span.classList.add(
-      "bg-red-500",
-      "h-2",
-      "w-2",
-      "rounded-full",
-      "absolute",
-      "top-4",
-      "left-4",
-    );
-
-    const board = document.getElementById("board");
-    board.appendChild(span);
-  }
-}
-
-class Garden {
-  constructor() {
-    this.id = "garden_" + Date.now().toString(36).substring(2, 9);
-    this.tilled = false;
-    this.occupied = false;
-
-    const div = document.createElement("div");
-    div.id = this.id;
-    div.classList.add("bg-green-500");
-
-    const board = document.getElementById("board");
-    board.appendChild(div);
-  }
-
-  till() {
-    if (this.tilled) {
-      console.error("Already tilled!");
-      return;
-    }
-    this.tilled = true;
-
-    const garden = document.getElementById(this.id);
-    garden.classList.remove("bg-green-500");
-    garden.classList.add("bg-brown-500");
-  }
-}
-
-class Board {
-  constructor() {
-    this.width = 1;
-    this.height = 1;
-    this.gardens = [];
-    this.occupied_garden = null;
-    this.player = null;
-
-    const board = document.createElement("div");
-
-    board.id = "board";
-    board.classList.add("grid", "grid-board", "gap-2", "relative");
-
-    const body = document.getElementById("body");
-    body.appendChild(board);
-
-    for (let i = 0; i < this.width * this.height; i++) {
-      this.gardens.push(new Garden());
-    }
-  }
-
-  set_size(width, height) {
-    this.width = width;
-    this.height = height;
-    document.documentElement.style.setProperty("--board-width", width);
-    document.documentElement.style.setProperty("--board-height", height);
-  }
-
-  increase() {
-    this.width += 1;
-    this.height += 1;
-    document.documentElement.style.setProperty("--board-width", this.width);
-    document.documentElement.style.setProperty("--board-height", this.height);
-  }
-
-  add_garden(garden) {
-    if (this.gardens.length >= this.width * this.height - 1) {
-      console.error("Board is full!");
-      return;
-    }
-    if (this.occupied_garden) {
-      this.gardens.push(garden);
-      return;
-    }
-
-    this.occupied_garden = garden;
-  }
-
-  spawn_player(player) {
-    if (this.player) {
-      console.error("Player already exists!");
-      return;
-    }
-    this.player = player;
-  }
-
-  // TODO:
-  move_east() {
-    for (let i = 0; i < this.gardens.length; i++) {
-      if (this.gardens[i].occupied) {
-        this.gardens[i].unoccupy();
-        this.gardens[i + 1].occupy();
-        break;
-      }
-    }
-    const player = document.getElementById("player");
-    player.classList.remove("left-4");
-    player.classList.add("left-8");
-  }
-
-  till() {
-    if (!this.occupied_garden) {
-      console.error("No garden to be tilled!");
-      return;
-    }
-    this.occupied_garden.till();
-  }
-}
-
 /**
  * Represents an iterator object.
+ * @template T
  */
 class Iter {
   /**
    * Creates an Iterator object.
-   * @param {Array} val - The array to iterate over.
+   * @param {Array<T>} val - The array to iterate over.
    */
   constructor(val) {
     /** @type {number} Current index of the iterator */
     this.idx = 0;
-    /** @type {Array} The array to iterate over */
-    this.val = val;
-    /** @type {Array} Array of higher order functions to be applied on the values */
-    this.higherOrder = [];
-    /** @type {Array} Array of callback functions */
+    /** @type {Array<T>} The array to iterate over */
+    this.val = val || [];
+    /** @type {Array<function(function(T): any, T): T | undefined>} Array of higher order functions to be applied on the values */
+    this.higher_order = [];
+    /** @type {Array<function(T): any>} Array of callback functions */
     this.callback = [];
   }
 
   /**
-   * Checks if there are more elements in the array to iterate over.
-   * @returns {boolean} True if there are more elements, false otherwise.
+   * Resets the iterator to its initial state.
+   * @returns {void}
    */
-  hasNext() {
+  reset() {
+    this.idx = 0;
+    this.higher_order = [];
+    this.callback = [];
+  }
+
+  /**
+   * Checks if there are next elements in the array to iterate over.
+   * @returns {boolean} True if there are next elements, false otherwise.
+   */
+  has_next() {
     return this.idx < this.val.length;
   }
 
   /**
    * Retrieves the next element from the array.
-   * @returns {*} The next element in the array.
+   * @returns {T} Next element in the array.
    */
   next() {
-    return this.val[this.idx++];
+    /** @type {T | undefined} The next element to be returned, or undefined if none. */
+    let next;
+
+    while (this.has_next() && next === undefined) {
+      next = this.val[this.idx++];
+      for (let i = 0; i < this.higher_order.length && next !== undefined; i++) {
+        next = this.higher_order[i](this.callback[i], next);
+      }
+    }
+    if (this.has_next()) {
+      console.assert(next !== undefined, "Iterator exhausted abruptly!");
+    }
+    return next;
+  }
+
+  /**
+   * Checks if there are previous elements in the array to iterate over.
+   * @returns {boolean} True if there are previous elements, false otherwise.
+   */
+  has_prev() {
+    return this.idx > 0;
+  }
+
+  /**
+   * Retrieves the previous element from the array.
+   * @returns {T} Previous element in the array.
+   */
+  prev() {
+    /** @type {T | undefined} The previous element to be returned, or undefined if none. */
+    let prev;
+
+    while (this.has_prev() && prev === undefined) {
+      prev = this.val[(this.idx -= 1)];
+      for (let i = 0; i < this.higher_order.length && prev !== undefined; i++) {
+        prev = this.higher_order[i](this.callback[i], prev);
+      }
+    }
+    if (this.has_prev()) {
+      console.assert(prev !== undefined, "Iterator exhausted abruptly!");
+    }
+    return prev;
   }
 
   /**
    * Adds a map function to be applied to the array.
-   * @param {Function} fn - The map function to add.
-   * @returns {Iter} - The Iterator object.
+   * @param {function(T): T} fn - The map function to add.
+   * @returns {Iter<T>} - The Iterator object.
    */
   map(fn) {
-    this.higherOrder.push(this._map);
+    this.higher_order.push(this._map);
     this.callback.push(fn);
     return this;
   }
 
   /**
    * Maps a function over the array.
-   * @param {Function} fn - The function to map.
-   * @param {*} val - The value to apply the function to.
+   * @param {function(T): T} fn - The function to map.
+   * @param {T} val - The value to apply the function to.
+   * @returns {T}
    * @private
    */
   _map(fn, val) {
-    // console.log(val);
-    // console.log(fn);
     return fn(val);
   }
 
   /**
    * Adds a filter function to be applied to the array.
-   * @param {Function} fn - The filter function to add.
-   * @returns {Iter} - The Iterator object.
+   * @param {function(T): boolean} fn - The filter function to add.
+   * @returns {Iter<T>} - The Iterator object.
    */
   filter(fn) {
-    this.higherOrder.push(this._filter);
+    this.higher_order.push(this._filter);
     this.callback.push(fn);
     return this;
   }
 
   /**
    * Filters the array based on the provided filter function.
-   * @param {Function} fn - The filter function.
-   * @param {*} val - The value to filter.
+   * @param {function(T): boolean} fn - The filter function.
+   * @param {T} val - The value to filter.
+   * @returns {T | undefined}
    * @private
    */
   _filter(fn, val) {
-    // console.log(val);
-    // console.log(fn);
     return fn(val) ? val : undefined;
   }
 
   /**
+   * Adds a tap function to be applied to the array.
+   * @param {function(T): void} fn - The tap function to add.
+   * @returns {Iter<T>} - The Iterator object.
+   */
+  tap(fn) {
+    this.higher_order.push(this._tap);
+    this.callback.push(fn);
+    return this;
+  }
+
+  /**
+   * Taps the array based on the provided tap function.
+   * @param {function(T): void} fn - The tap function.
+   * @param {T} val - The value to tap.
+   * @returns {T}
+   * @private
+   */
+  _tap(fn, val) {
+    fn(val);
+    return val;
+  }
+
+  /**
    * Collects the modified elements from the iterator.
-   * @returns {Array} - The collected elements.
+   * @returns {Array<T>} - The collected elements.
    */
   collect() {
-    let full_boxed = [];
-    while (this.hasNext()) {
-      let boxed = this.next();
-      for (let i = 0; i < this.higherOrder.length && boxed !== undefined; i++) {
-        boxed = this.higherOrder[i](this.callback[i], boxed);
+    /** @type {Array<T>} The array to collect the results. */
+    let collected = [];
+
+    while (this.has_next()) {
+      /** @type {T | undefined} The transformed element to be collected, or undefined if none. */
+      let transformed = this.next();
+
+      if (transformed !== undefined) {
+        collected.push(transformed);
       }
-      boxed !== undefined && full_boxed.push(boxed);
     }
-    return full_boxed;
+    return collected;
   }
 }
 
 const items = [1, 2, 8];
-const iterator = new Iter(items);
-console.log(
-  "Result: ",
-  iterator
-    .map((val) => val + 1)
-    .filter((val) => val === 3)
-    .map((val) => val + 2)
-    .collect(),
-);
+const iterator1 = new Iter(items);
+const iterator2 = new Iter(items);
+
+iterator1.next();
+iterator1.prev();
+iterator1.map((val) => val + 1);
+
+iterator2
+  .map((val) => val + 1)
+  .tap((val) => console.log("tapping1", val))
+  .map((val) => val + 2)
+  .tap((val) => console.log("tapping2", val));
+
+const collected1 = iterator1.collect();
+const collected2 = iterator2.collect();
+
+console.log("Collected 1:", collected1);
+console.log("Collected 2:", collected2);
